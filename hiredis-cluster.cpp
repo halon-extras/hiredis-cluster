df35a23dc114aee8fda1a090514188005f8e4298
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2022-01-12 14:19:26 +0000

    Try to resolve hostname again if command fails

--- hiredis-cluster.cpp
+++ hiredis-cluster.cpp
@@ -10,11 +10,48 @@ std::queue<redisClusterContext*> pool;
 std::condition_variable cv;
 std::mutex mutex;
 hiredisAllocFuncs hiredisAllocFns;
+std::string nodes;
+std::string password;
+std::string connect_timeout;
+std::string timeout;
+std::string max_retry;
 
 HALON_EXPORT
 int Halon_version()
 {
 	return HALONMTA_PLUGIN_VERSION;
+}
+
+redisClusterContext* redisClusterContextInit2()
+{
+	redisClusterContext *cc = redisClusterContextInit();
+
+	if (!connect_timeout.empty())
+	{
+		struct timeval tv = { strtol(connect_timeout.c_str(), nullptr, 10) , 0 };
+		redisClusterSetOptionConnectTimeout(cc, tv);
+	}
+	if (timeout.empty())
+	{
+		struct timeval tv = { strtol(timeout.c_str(), nullptr, 10) , 0 };
+		redisClusterSetOptionTimeout(cc, tv);
+	}
+	if (!max_retry.empty())
+	{
+		redisClusterSetOptionMaxRetry(cc, strtol(max_retry.c_str(), nullptr, 10));
+	}
+	if (!password.empty())
+	{
+		redisClusterSetOptionPassword(cc, password.c_str());
+	}
+
+	int ret = redisClusterSetOptionAddNodes(cc, nodes.c_str());
+	if (ret != REDIS_OK)
+	{
+		throw std::runtime_error("bad nodes");
+	}
+
+	return cc;
 }
 
 HALON_EXPORT
@@ -28,13 +65,18 @@ bool Halon_init(HalonInitContext* hic)
 		return false;
 
 	const char* pool_size_ = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "pool_size"), nullptr);
-	const char* nodes = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "nodes"), nullptr);
-	const char* password = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "password"), nullptr);
-	const char* connect_timeout = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "connect_timeout"), nullptr);
-	const char* timeout = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "timeout"), nullptr);
-	const char* max_retry = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "max_retry"), nullptr);
+	const char* nodes_ = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "nodes"), nullptr);
+	if (nodes_) nodes = nodes_;
+	const char* password_ = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "password"), nullptr);
+	if (password_) password = password_;
+	const char* connect_timeout_ = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "connect_timeout"), nullptr);
+	if (connect_timeout_) connect_timeout = connect_timeout_;
+	const char* timeout_ = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "timeout"), nullptr);
+	if (timeout_) timeout = timeout_;
+	const char* max_retry_ = HalonMTA_config_string_get(HalonMTA_config_object_get(cfg, "max_retry"), nullptr);
+	if (max_retry_) max_retry = max_retry_;
 
-	if (!nodes)
+	if (nodes.empty())
 	{
 		syslog(LOG_CRIT, "hiredis-cluster: no nodes");
 		return false;
@@ -43,31 +85,11 @@ bool Halon_init(HalonInitContext* hic)
 	size_t pool_size = pool_size_ ? strtoul(pool_size_, nullptr, 10) : 1;
 	for (size_t i = 0; i < pool_size; ++i)
 	{
-		redisClusterContext *cc = redisClusterContextInit();
-
-		if (connect_timeout)
-		{
-			struct timeval tv = { strtol(connect_timeout, nullptr, 10) , 0 };
-			redisClusterSetOptionConnectTimeout(cc, tv);
-		}
-		if (timeout)
-		{
-			struct timeval tv = { strtol(timeout, nullptr, 10) , 0 };
-			redisClusterSetOptionTimeout(cc, tv);
-		}
-		if (max_retry)
-		{
-			redisClusterSetOptionMaxRetry(cc, strtol(max_retry, nullptr, 10));
-		}
-		if (password)
-		{
-			redisClusterSetOptionPassword(cc, password);
-		}
-
-		int ret = redisClusterSetOptionAddNodes(cc, nodes);
-		if (ret != REDIS_OK)
-		{
-			syslog(LOG_CRIT, "hiredis-cluster: bad nodes");
+		redisClusterContext *cc;
+		try {
+			cc = redisClusterContextInit2();
+		} catch (const std::runtime_error& e) {
+			syslog(LOG_CRIT, "hiredis-cluster: %s", e.what());
 			return false;
 		}
 		redisClusterConnect2(cc);
@@ -132,6 +154,13 @@ void HSLRedisClusterCommand(HalonHSLContext* hhc, HalonHSLArguments* args, Halon
 	redisReply* reply = (redisReply*)redisClusterCommandArgv(cc, argv.size(), &argv[0], &lens[0]);
 	if (!reply)
 	{
+		redisClusterFree(cc);
+		try {
+			cc = redisClusterContextInit2();
+		} catch (const std::runtime_error& e) {
+			set_ret_error(ret, e.what());
+			goto add_back;
+		}
 		redisClusterConnect2(cc);
 		if (cc->err)
 		{
